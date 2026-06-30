8d20b8839df7137b7f4ad941c7529988839910db
JitDun <cal.dun08@gmail.com>
2026-06-30 16:45:27 +0800

    Testing API

--- main.py
+++ main.py
@@ -9,10 +9,18 @@ def main():
     if api_key is None:
         raise RuntimeError("Error: no API key found")
     
+    test_contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(model="gemini-2.5-flash", 
-                contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-    print(response.text)
+                contents = test_contents)
+
+    if (response.usage_metadata is None):
+        raise RuntimeError("Error: Gemini response failed")
+    
+    print(f"User prompt: {test_contents}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response:\n{response.text}")
 
 
 if __name__ == "__main__":
